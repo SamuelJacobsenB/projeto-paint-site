0c47cec3002aea0a009f56751f803756d7e93d46
Samuel Jacobsen Barcellos <rcbsjbarcellos@gmail.com>
2025-01-30 07:30:07 -0300

    (front) feat: Criando a class Controller para lidar dinamicamente com chamadas a api

--- apps/frontend/src/services/controller.service.ts
+++ apps/frontend/src/services/controller.service.ts
@@ -13,4 +13,48 @@ export class Controller {
 
     return { data: response.data };
   }
+
+  async post<DataType>(
+    url: string,
+    data?: DataType,
+    access_token?: string
+  ): Promise<Response> {
+    const response = await api.post(url, data, {
+      headers: { Authorization: access_token },
+    });
+
+    if (response.status >= 400) {
+      return { error: response.statusText };
+    }
+
+    return { data: response.data };
+  }
+
+  async patch<DataType>(
+    url: string,
+    data?: DataType,
+    access_token?: string
+  ): Promise<Response> {
+    const response = await api.patch(url, data, {
+      headers: { Authorization: access_token },
+    });
+
+    if (response.status >= 400) {
+      return { error: response.statusText };
+    }
+
+    return { data: response.data };
+  }
+
+  async delete(url: string, access_token?: string): Promise<Response> {
+    const response = await api.delete(url, {
+      headers: { Authorization: access_token },
+    });
+
+    if (response.status >= 400) {
+      return { error: response.statusText };
+    }
+
+    return { data: response.data };
+  }
 }
